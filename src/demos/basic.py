423202888947e5990b47b67e1a8f9610e562a43c
John Morris <70734743+jmorris335@users.noreply.github.com>
2024-08-14 14:15:20 -0400

    Initialize README demo

---
+++ src/demos/basic.py
@@ -0,0 +1,21 @@
+from src.main.hypergraph import Hypergraph, Relationship
+
+negation_rel = Relationship('negate#rel', lambda source : -source[0])
+plus_rel = Relationship('plus#rel', lambda source : sum(source))
+
+hg = Hypergraph()
+hg.addEdge(['A', 'B'], 'C', plus_rel)
+hg.addEdge('A', 'D', negation_rel)
+hg.addEdge('B', 'E', negation_rel)
+hg.addEdge(['D', 'E'], 'F', plus_rel)
+hg.addEdge('F', 'C', negation_rel)
+
+print("\n**Inputs A and B**")
+hg.solve(['A', 'B'], toPrint=True)
+print("\n**Inputs A and E**")
+hg.solve(['A', 'E'], toPrint=True)
+
+print("\n**Inputs A and B**")
+hg(input_values=dict(A=3, B=7), target='C', toPrint=True)
+print("\n**Inputs A and E**")
+hg(input_values=dict(A=3, E=-7), target='C', toPrint=True)
